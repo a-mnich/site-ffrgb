9ac9d008255a380b43dba8ffb3ffbf80ac843e28
Alexander Mnich <56564725+a-mnich@users.noreply.github.com>
2025-05-05 22:30:32 +0000

    migrate to image-customization.lua

---
+++ image-customization.lua
@@ -0,0 +1,24 @@
+packages {
+	'gluon-segment-mover',
+	'iwinfo',
+	'gluon-ssid-changer',
+	'respondd-module-airtime',
+	'gluon-mesh-vpn-wireguard-vxlan',
+	'ffrgb-banner'
+}
+
+features {
+	'autoupdater',
+	'config-mode-domain-select',
+	'config-mode-geo-location-osm',
+	'config-mode-mesh-vpn',
+	'ebtables-filter-multicast',
+	'ebtables-filter-ra-dhcp',
+	'ebtables-source-filter',
+	'mesh-batman-adv-15',
+	'respondd',
+	'status-page',
+	'web-advanced',
+	'web-private-wifi',
+	'web-wizard'
+}
--- site.mk
+++ site.mk
@@ -1,31 +1,6 @@
 GLUON_MULTIDOMAIN=1
 GLUON_DEPRECATED ?= upgrade
 
-GLUON_FEATURES := \
-	autoupdater \
-	config-mode-domain-select \
-	config-mode-geo-location-osm \
-	config-mode-mesh-vpn \
-	ebtables-filter-multicast \
-	ebtables-filter-ra-dhcp \
-	ebtables-source-filter \
-	mesh-batman-adv-15 \
-	respondd \
-	status-page \
-	web-advanced \
-	web-private-wifi \
-	web-wizard
-
-
-# base packages
-GLUON_SITE_PACKAGES := \
-	gluon-segment-mover \
-	iwinfo \
-	gluon-ssid-changer \
-	respondd-module-airtime \
-	gluon-mesh-vpn-wireguard-vxlan \
-	ffrgb-banner 
-
 DEFAULT_GLUON_RELEASE := snapshot~$(shell date '+%Y%m%d')
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
